069679cab9404748c2b726bfbb84907d0e85fdf6
e-box <e-box@live.ru>
2023-08-04 20:41:08 +0800

    HW #2 finished

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product, Contacts
+
+
+@admin.register(Category)
+class AdminCategory(admin.ModelAdmin):
+    list_display = ('id', 'title')
+
+
+@admin.register(Product)
+class AdminProduct(admin.ModelAdmin):
+    list_display = ('id', 'title', 'price', 'category')
+    list_filter = ('category', )
+    search_fields = ('title', 'description')
+
+
+@admin.register(Contacts)
+class AdminContacts(admin.ModelAdmin):
+    list_display = ('address', 'phone_number')
--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,47 @@
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='media/', verbose_name='изображение', **NULLABLE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='категория товара')
+    price = models.IntegerField(verbose_name='цена')
+    created_at = models.DateField(verbose_name='дата создания', default=timezone.now())
+    changed_at = models.DateField(verbose_name='дата изменения', default=timezone.now())
+
+    def __str__(self):
+        return f'{self.title} из категории {self.category}'
+
+    class Meta:
+        verbose_name = 'продукт'
+        verbose_name_plural = 'продукты'
+        ordering = ('changed_at',)
+
+
+class Contacts(models.Model):
+    phone_number = models.CharField(max_length=20, verbose_name='номер')
+    address = models.TextField(verbose_name='адрес')
+
+    def __str__(self):
+        return f'Адрес: {self.address}' \
+               f'Телефон для связи: {self.phone_number}'
+
+    class Meta:
+        verbose_name = 'контакт'
+        verbose_name_plural = 'контакты'
