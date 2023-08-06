271b269e2a40abc9b2865b5eceb086a70a2d5843
e-box <e-box@live.ru>
2023-08-06 21:49:30 +0800

    HW #3 finished. Created some ner views, templates, tags

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,9 +2,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
-from catalog.views import index, contacts
+from catalog.views import index, contacts, item_info
 
 urlpatterns = [
     path('', index),
-    path('contacts/', contacts)
+    path('contacts/', contacts),
+    path('item_info/<int:pk>/', item_info)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- catalog/views.py
+++ catalog/views.py
@@ -2,13 +2,15 @@ from django.shortcuts import render
 from catalog.models import Product, Contacts
 
 
-# Create your views here.
 def index(request):
     # Вывод  в консоль последних пяти добавленных товаров
     lst = Product.objects.all().order_by('pk').reverse()[:5]
     print(*lst, sep='\n')
+    context = {
+        'items': Product.objects.all()
+    }
 
-    return render(request, 'catalog/index.html')
+    return render(request, 'catalog/index.html', context)
 
 
 def contacts(request):
@@ -22,3 +24,10 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'You have new message from {name} ({phone}): {message}')
     return render(request, 'catalog/contacts.html', context)
+
+
+def item_info(request, pk):
+    context = {
+        'item': Product.objects.get(pk=pk)
+    }
+    return render(request, 'catalog/item_info.html', context)
